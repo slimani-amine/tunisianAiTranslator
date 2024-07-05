c588f705c4a4044ee9806cac4170bda3874ec4b7
slimani-amine <slimanimedamine24@gmail.com>
2024-07-05 10:35:17 +0100

    FIX: some style

--- components/ui/textarea.jsx
+++ components/ui/textarea.jsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full text-black rounded-md border border-black/20 shadow-xl bg-white/80 px-4 py-5 text-base placeholder:text-black/60 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-accent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 ",
+        "flex min-h-[80px] w-full text-black rounded-md border border-black/20 shadow-xl bg-white px-4 py-5 text-base placeholder:text-black/60 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-accent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 ",
         className
       )}
       ref={ref}
