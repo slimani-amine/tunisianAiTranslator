3d8add3f56b93c36cf181acdce9c9e8e7b463c76
slimani-amine <slimanimedamine24@gmail.com>
2024-07-05 10:34:13 +0100

    FIX: some style

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type}
       className={cn(
-        "flex w-full h-[48px] text-black rounded-md shadow-xl border border-black/20 bg-white/80 focus:border-accent font-light bg-white px-4 py-5 text-base placeholder:text-white/60 outline-none ",
+        "flex w-full h-[48px] text-black rounded-md shadow-md border border-black/20 bg-white/80 focus:border-accent font-light bg-white px-4 py-5 text-base placeholder:text-white/60 outline-none ",
         className
       )}
       ref={ref}
